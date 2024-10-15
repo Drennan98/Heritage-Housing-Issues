70941a0347539b538f8ea2aff7e22036bbd5a1a7
Darragh Drennan <darraghdrennan5@gmail.com>
2024-10-15 08:44:31 +0000

    Design summary page

--- app_pages/page1_summary.py
+++ app_pages/page1_summary.py
@@ -18,5 +18,34 @@ def main():
     elif page == "Model Performance":
         model_performance()
 
+# The actual layout of this summary page was taken from the Churnometer walkthrough project. 
+
 def page1_summary():
-    pass
+
+    st.write("### Quick Project Summary")
+
+    # text based on README file - "Dataset Content" section
+    st.info(
+        f"**Project Terms & Jargon**\n"
+        f"* The **sale price** refers to the numerical value (in US Dollars) that a house sells for.\n "
+        f"**Project Dataset**\n"
+        f"* The dataset for this project is available on [Kaggle](https://www.kaggle.com/datasets/codeinstitute/housing-prices-data)."
+    )
+
+    # Link to README file, so the users can have access to full project documentation
+    st.write(
+        f"* For additional information, please visit and **read** the "
+        f"[Project README file](https://github.com/Drennan98/Heritage-Housing-Issues).")
+    
+
+    # copied from README file - "Business Requirements" section
+    st.success(
+        f"#### The project has two business requirements:\n\n"
+        f"**1.**  The client is interested in discovering how house attributes correlate with "
+        f"the house Sale Price. Therefore, the client expects data visualizations "
+        f"of the correlated variables against Sale Price to show that.\n\n"
+        f"**2.** The client is interested to predict the house sales price from their 4 "
+        f"inherited houses, and any other house in Ames, Iowa. "
+        )
+
+        
